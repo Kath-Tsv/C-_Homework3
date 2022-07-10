7c469e64b2e468da4731064e771b9bd706249e41
Katherine <kath.tsv@yandex.ru>
2022-07-10 21:53:50 +0300

    Homework3 is ready

---
+++ Task_19/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine ("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine ());
+Task19 (number);
+
+void Task19(int value)
+{
+    if (9999<value && value<100000)
+    {
+        int first = value/10000;
+        Console.WriteLine ($"first = {first}");
+        int second = value%10000/1000;
+        Console.WriteLine ($"second = {second}");
+        int fourth = value%100/10;
+        Console.WriteLine ($"fourth = {fourth}");
+        int fith = value%10;
+        Console.WriteLine ($"fith = {fith}");
+        if (first == fith && second == fourth)
+        {
+            Console.WriteLine ($"Число {value} является палиндромом.");
+        }
+        else 
+        {
+            Console.WriteLine ($"Число {value} не является палиндромом.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine ($"Число {value} не является пятизначным.");
+    }
+}
---
+++ Task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double xA = 1;
+double yA = 10;
+double zA = 15;
+double xB = 2;
+double yB = 20;
+double zB = 25;
+
+Task21 (xA, yA, zA, xB, yB, zB);
+
+void Task21 (double xA, double yA, double zA, double xB, double yB, double zB)
+{
+   double distance = Math.Sqrt((Math.Pow(xA-xB, 2) + Math.Pow(yA-yB, 2) + Math.Pow(zA-zB, 2)));
+    Console.WriteLine ($"Расстояние между точками А ({xA}, {yA}, {zA}) и В ({xB}, {yB}, {zB}) равно: {distance}");
+}
